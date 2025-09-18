13208a0aaa1cdd5513fc469bb21eafd4c767bd07
ObjectGipi <sound.of.olga@gmail.com>
2025-09-18 10:31:32 +0000

    feat: big updategit log add C-coding-test-project intergrate with TS-coding-test-project in VSCode IDE

---
+++ C/src/20250918.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// char 최대 입력 크기 (문자열 길이 + '\0')
+#define LEN_INPUT 1000001
+
+int main(void) {
+  char s1[LEN_INPUT];
+
+  // 문자열 입력 (공백 전까지만 읽음)
+  scanf("%s", s1);
+
+  // 문자열 출력
+  printf("%s\n", s1);
+
+  // 프로그램 정상 종료
+  return 0;
+}
