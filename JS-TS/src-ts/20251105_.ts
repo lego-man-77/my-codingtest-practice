20ce976428e125a8ebbaaf179890c83c149b4dc1
ObjectGipi <sound.of.olga@gmail.com>
2025-11-05 19:35:29 +0900

    20251105

--- JS-TS/src-ts/20251105_.ts
+++ JS-TS/src-ts/20251105_.ts
@@ -1,79 +1,66 @@
 /**
- * 문제 설명:
- * 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때,
- * my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을
- * return 하도록 solution 함수를 완성해보세요.
+ * 문제 설명
+ * 머쓱이는 행운의 숫자 7을 가장 좋아합니다.
+ * 정수 배열 array가 매개변수로 주어질 때,
+ * 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
  *
- * 제한사항:
- * 0 < my_string 길이 < 100
+ * 제한사항
+ * 1 ≤ array의 길이 ≤ 100
+ * 0 ≤ array의 원소 ≤ 100,000
  *
- * 입출력 예:
- * my_string     result
- * "Bcad"        "abcd"
- * "heLLo"       "ehllo"
- * "Python"      "hnopty"
+ * 입출력 예
+ * array	        result
+ * [7, 77, 17]	    4
+ * [10, 29]	    0
  *
- * 입출력 예 설명:
- * 예 #1: "Bcad" → "bcad" → "abcd"
- * 예 #2: "heLLo" → "hello" → "ehllo"
- * 예 #3: "Python" → "python" → "hnopty"
+ * 입출력 예 설명
+ * 입출력 예 #1
+ * [7, 77, 17]에는 7이 4개 있으므로 4를 return 합니다.
+ *
+ * 입출력 예 #2
+ * [10, 29]에는 7이 없으므로 0을 return 합니다.
  */
-const sortStringToLowercase = (myString: string): string => {
-  // 1. myString 각 문자를 ascii code로 변환 -> .charCodeAt(index)
-  // 2. 만약 65 ~ 90 범위의 숫자는 "+ 32" ("A" ~ "Z")
-  // 3. 변환한 숫자를 오름차순으로 정렬
-  // 4. 정렬된 숫자를 순차적으로 문제로 변환 -> String.fromCharCode()
-  let numberArray = [];
-  let result = ``;
-  console.log(`주어진 문자열: ${myString}`);
-  for (let i = 0; i < myString.length; i++) {
-    let stringToNumber = myString.charCodeAt(i);
-    if (stringToNumber >= 65 && stringToNumber <= 90) {
-      stringToNumber = stringToNumber + 32;
+const calLuckySeven = (array: number[]): number => {
+  // 1. array의 모든 원소를 문자열로 합침
+  // 2. 해당 문자열을 순회하며 7의 갯수 계산하여 반환
+  let result = 0;
+  const arrayToString = array.join(``);
+  for (let i = 0; i < arrayToString.length; i++) {
+    if (arrayToString[i] === "7") {
+      result = result + 1;
     }
-    numberArray.push(stringToNumber);
   }
-
-  console.log(`문자열을 숫자로 변환: ${numberArray}`);
-  numberArray.sort((a, b) => a - b);
-  console.log(`오름차순으로 정렬: ${numberArray}`);
-
-  for (let i = 0; i < numberArray.length; i++) {
-    result = result + String.fromCharCode(numberArray[i]);
-  }
-
-  console.log(`숫자를 문자열로 변환: ${numberArray}`);
-  console.log(`정렬된 최종 문자열: ${result}`);
   return result;
 };
 
-console.log(sortStringToLowercase("Bcad")); // "abcd"
-console.log(sortStringToLowercase("heLLo")); // "ehllo"
-console.log(sortStringToLowercase("Python")); // "hnopty"
-console.log(`========================`);
+console.log(calLuckySeven([7, 77, 17])); // 4
+console.log(calLuckySeven([10, 29])); // 0
+console.log(calLuckySeven([0])); // 0
 
 // 이슈⚠️
 // 코드 리팩토링
 
-const sortStringToLowercaseV2 = (myString: string): string => {
-  return [...myString] // ➡️ ["B", "c", "a", "d]
-    .map((element) => element.toLowerCase()) // ➡️ ["b", "c", "a", "d]
-    .sort() // ➡️ ["a", "b", "c", "d]
-    .join(``);// ➡️"abcd"
-};
+const calLuckySevenV2 = (array: number[]): number => {
+  return array.join(``).split("7").length - 1;
+  // 문자열.split("7").length - 1 ➡️ 7의 갯수를 의미
+}
 
-console.log(sortStringToLowercaseV2("Bcad")); // "abcd"
-console.log(sortStringToLowercaseV2("heLLo")); // "ehllo"
-console.log(sortStringToLowercaseV2("Python")); // "hnopty"
+const calLuckySevenV3 = (array: number[]): number => {
+  return array.reduce((sum, e) => sum + (String(e).split("7").length - 1), 0);
+  // sum의 초기값 0
+  // array의 모든 원소(e)를 사용해서 각 원소를 돌때마다
+  // sum + (String(e).split("7").length - 1) 실행
+}
+
+console.log(calLuckySevenV2([7, 77, 17])); // 4
+console.log(calLuckySevenV2([10, 29])); // 0
+console.log(calLuckySevenV2([0])); // 0
+console.log(calLuckySevenV3([7, 77, 17])); // 4
+console.log(calLuckySevenV3([10, 29])); // 0
+console.log(calLuckySevenV3([0])); // 0
 
 // 해결⛑️
 // 아래 함수들로 리팩토링
-// [...args] 구조 분해 할당
-// map(() => {})
-// toLowerCase()
-// sort()
 // join()
-
-// 추가 공부하면 좋은 함수📚
-// toUpperCase()
-// reverse()
+// split()
+// reduce(() => {})
---
+++ JS-TS/src-ts/20251105_sortStringToLowercase.ts
@@ -0,0 +1,79 @@
+/**
+ * 문제 설명:
+ * 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때,
+ * my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을
+ * return 하도록 solution 함수를 완성해보세요.
+ *
+ * 제한사항:
+ * 0 < my_string 길이 < 100
+ *
+ * 입출력 예:
+ * my_string     result
+ * "Bcad"        "abcd"
+ * "heLLo"       "ehllo"
+ * "Python"      "hnopty"
+ *
+ * 입출력 예 설명:
+ * 예 #1: "Bcad" → "bcad" → "abcd"
+ * 예 #2: "heLLo" → "hello" → "ehllo"
+ * 예 #3: "Python" → "python" → "hnopty"
+ */
+const sortStringToLowercase = (myString: string): string => {
+  // 1. myString 각 문자를 ascii code로 변환 -> .charCodeAt(index)
+  // 2. 만약 65 ~ 90 범위의 숫자는 "+ 32" ("A" ~ "Z")
+  // 3. 변환한 숫자를 오름차순으로 정렬
+  // 4. 정렬된 숫자를 순차적으로 문제로 변환 -> String.fromCharCode()
+  let numberArray = [];
+  let result = ``;
+  console.log(`주어진 문자열: ${myString}`);
+  for (let i = 0; i < myString.length; i++) {
+    let stringToNumber = myString.charCodeAt(i);
+    if (stringToNumber >= 65 && stringToNumber <= 90) {
+      stringToNumber = stringToNumber + 32;
+    }
+    numberArray.push(stringToNumber);
+  }
+
+  console.log(`문자열을 숫자로 변환: ${numberArray}`);
+  numberArray.sort((a, b) => a - b);
+  console.log(`오름차순으로 정렬: ${numberArray}`);
+
+  for (let i = 0; i < numberArray.length; i++) {
+    result = result + String.fromCharCode(numberArray[i]);
+  }
+
+  console.log(`숫자를 문자열로 변환: ${numberArray}`);
+  console.log(`정렬된 최종 문자열: ${result}`);
+  return result;
+};
+
+console.log(sortStringToLowercase("Bcad")); // "abcd"
+console.log(sortStringToLowercase("heLLo")); // "ehllo"
+console.log(sortStringToLowercase("Python")); // "hnopty"
+console.log(`========================`);
+
+// 이슈⚠️
+// 코드 리팩토링
+
+const sortStringToLowercaseV2 = (myString: string): string => {
+  return [...myString] // ➡️ ["B", "c", "a", "d]
+    .map((element) => element.toLowerCase()) // ➡️ ["b", "c", "a", "d]
+    .sort() // ➡️ ["a", "b", "c", "d]
+    .join(``); // ➡️"abcd"
+};
+
+console.log(sortStringToLowercaseV2("Bcad")); // "abcd"
+console.log(sortStringToLowercaseV2("heLLo")); // "ehllo"
+console.log(sortStringToLowercaseV2("Python")); // "hnopty"
+
+// 해결⛑️
+// 아래 함수들로 리팩토링
+// [...args] 구조 분해 할당
+// map(() => {})
+// toLowerCase()
+// sort()
+// join()
+
+// 추가 공부하면 좋은 함수📚
+// toUpperCase()
+// reverse()
